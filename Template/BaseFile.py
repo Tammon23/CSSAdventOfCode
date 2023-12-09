4cbf685b78cc4b86ee9d729e37296a408cac8894
Tammon23 <Tammon2000@gmail.com>
2023-12-09 00:36:09 -0500

    updated BaseFile.py

--- Template/BaseFile.py
+++ Template/BaseFile.py
@@ -4,18 +4,18 @@ file = "example_input.txt"
 # file = "input.txt"
 
 
-def clean_input(t: str) -> str:
-    return t.strip("\n")
+def clean_input(t: str) -> Any:
+    return t
 
 
-def SomeFunction(data: Iterable[str]) -> Any:
+def solve(data: Iterable[str]) -> Any:
     return data
 
 
 if __name__ == "__main__":
     # reading in the input
     with open(file, "r") as f:
-        # inputs = map(clean_input, f.readlines())
-        inputs = f.readlines()
+        # inputs = map(clean_input, f.read().splitlines())
+        inputs = f.read().splitlines()
 
-    print(SomeFunction(inputs))
+    print(solve(inputs))
