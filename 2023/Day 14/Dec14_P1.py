6948c98a4c5beff10ed87474d29e1c36af490095
Tammon23 <Tammon2000@gmail.com>
2023-12-14 21:22:30 -0500

    day 14 part 1 cleanup

--- 2023/Day 14/Dec14_P1.py
+++ 2023/Day 14/Dec14_P1.py
@@ -1,40 +1,27 @@
-from typing import Any, Iterable
+from typing import List
 
-# file = "example_input.txt"
-file = "input.txt"
-
-
-def clean_input(t: str) -> Any:
-    return t
+file = "example_input.txt"
+# file = "input.txt"
 
 
-def solve(data: Iterable[str]) -> Any:
-    # for row in data:
-    #     print("".join(row))
-    # print("-"*9)
+def solve(data: List[List[str]]) -> int:
     for r, row in enumerate(data):
         for c, val in enumerate(row):
             if val == "O":
                 next_r = r - 1
-                while next_r >= 0 and data[next_r][c]  == ".":
+                while next_r >= 0 and data[next_r][c] == ".":
                     next_r -= 1
 
-                data[next_r+1][c] = "O"
+                data[next_r + 1][c] = "O"
                 if r != next_r + 1:
                     data[r][c] = "."
 
-    total = 0
-    for r, row in enumerate(reversed(data), 1):
-        total += r * row.count("O")
-    # for row in data:
-    #     print("".join(row))
-    return total
+    return sum(r * row.count("O") for r, row in enumerate(reversed(data), 1))
 
 
 if __name__ == "__main__":
     # reading in the input
     with open(file, "r") as f:
-        # inputs = map(clean_input, f.read().splitlines())
-        inputs = [list(row) for row in f.read().splitlines()]
+        platform = [list(row) for row in f.read().splitlines()]
 
-    print(solve(inputs))
+    print(solve(platform))
