01805354478c63101775516bc6afd7935ab555ee
Tammon23 <Tammon2000@gmail.com>
2023-12-23 17:36:23 -0500

    Updated Helper.py
    - Added option to colour text
    - Updated pprint_array to optionally join each row now

--- Helper.py
+++ Helper.py
@@ -16,6 +16,21 @@ strs_to_directions = {
 }
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+def colourize(text: str, colour: bcolors=bcolors.FAIL) -> str:
+    return f"{colour}{text}\033[0m"
+
+
 def get_min_max_at_index(iterable: Iterable[Tuple[int, int]], index: int) -> Tuple[int, int]:
     return min(value[index] for value in iterable), max(value[index] for value in iterable)
 
@@ -32,9 +47,12 @@ def rotate_point_90ccw(point: Tuple[int, int]) -> Tuple[int, int]:
     return -point[1], point[0]
 
 
-def pprint_array(array: Iterable[Iterable[Any]]) -> None:
+def pprint_array(array: Iterable[Iterable[Any]], join_row=True, join_sep="") -> None:
     for row in array:
-        print(row)
+        if join_row:
+            print(join_sep.join(row))
+        else:
+            print(row)
 
 
 def add_tuple(tuple1: [Tuple[int, int]], tuple2: [Tuple[int, int]]) -> Tuple[int, int]:
