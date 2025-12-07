b6ae63e1631fe54b066be504bca2f2241600ffa8
Tammon23 <26889028+Tammon23@users.noreply.github.com>
2025-12-07 10:10:30 -0500

    Day 7

---
+++ 2025/Day 7/Dec7_P2.py
@@ -0,0 +1,28 @@
+from functools import lru_cache
+
+file = "example_input.txt"
+# file = "input.txt"
+
+@lru_cache
+def solve(row: int, col: int) -> int:
+    maxRow = len(inputs)
+    if col < 0 or col >= len(inputs[0]):
+        return 0
+
+    while row < maxRow:
+        if inputs[row][col] == ".":
+            row += 1
+
+        elif inputs[row][col] == "^":
+            return 1 + solve(row, col + 1) + solve(row, col - 1)
+
+    return 0
+
+
+if __name__ == "__main__":
+    # reading in the input
+    with open(file, "r") as f:
+        inputs = f.read().splitlines()
+
+    print(solve(1, inputs[0].index("S")) + 1)
+
